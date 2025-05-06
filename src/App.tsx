73e338e2a1a9b17f473ce24347f288a25ef3efcc
tuyenkim-2004 <kimtuyen10072004@gmail.com>
2025-05-06 22:38:13 +0700

    SPGTS-34, SPGTS-49: Done UI for the student's profile

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import './styles/App.css'
 import { BrowserRouter } from "react-router-dom";
 import ClassManagement from './pages/ClassManagement.tsx';
-
+import StudentProfilePage from './pages/StudentProfilePage.tsx';
+import StudentCard from './components/StudentCard'; // Đường dẫn đến component StudentCard
+import StudentProfile from './components/StudentProfile'; // Đường dẫn đến component StudentProfile
+import StudentGoal from './components/StudentGoal';
 const App = () => {
   return (
     <BrowserRouter>
-      <ClassManagement/>
+      {/* <ClassManagement/> */}
+      {/* <StudentCard
+        name="John Doe"
+        imageUrl="https://i.pinimg.com/736x/9e/92/c1/9e92c1bac422b24faa796fbfb37bb0d2.jpg"
+        onAccessClick={handleAccessClick}
+      /> */}
+      {/* <StudentProfile />  */}
+      {/* <StudentGoal /> */}
+      <StudentProfilePage/>
     </BrowserRouter>
   );
 }
