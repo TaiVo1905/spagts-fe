fb82220dc510c22710d50380bdeed05848c454ec
binhnexusx <binhhoangdev9@gmail.com>
2025-05-16 10:10:17 +0700

    SPGTS-38 SPGTS-55 Done UI Semester Goal

--- src/routes/StudentRoutes.tsx
+++ src/routes/StudentRoutes.tsx
@@ -1,13 +1,12 @@
 import { RouteObject } from "react-router-dom";
 import StudentProfilePage from "../pages/student/StudentProfilePage";
+import StudentSemesterGoalPage from "../pages/student/StudentSemesterGoalPage";
 
-export const studentRoutes: RouteObject[] =  [
-      { path: "profile", element: <StudentProfilePage /> },
-      { path: "achievement", element: <StudentProfilePage /> },
-      { path: "semester-goal", element: <StudentProfilePage /> },
-      { path: "learning-journal", element: <StudentProfilePage /> },
-      { path: "in-class", element: <StudentProfilePage /> },
-      { path: "self-study", element: <StudentProfilePage /> },
-    ]
-
-;
+export const studentRoutes: RouteObject[] = [
+  { path: "profile", element: <StudentProfilePage /> },
+  { path: "achievement", element: <StudentProfilePage /> },
+  { path: "semester-goal", element: <StudentSemesterGoalPage /> },
+  { path: "learning-journal", element: <StudentProfilePage /> },
+  { path: "in-class", element: <StudentProfilePage /> },
+  { path: "self-study", element: <StudentProfilePage /> },
+];
